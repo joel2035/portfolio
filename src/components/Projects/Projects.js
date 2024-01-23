f8a52395437d31154aa926f850bcf5bf27953aab
Joel <joel.pokam@hetic.net>
2024-01-23 14:12:58 +0100

    udapte

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -49,6 +49,9 @@ const Projects = () => (
               </TagList>
             </div>
             <UtilityList>
+              <ExternalLinks href={p.visit} target="_blank">
+                Live Preview
+              </ExternalLinks>
               <ExternalLinks href={p.source}>Source Code</ExternalLinks>
             </UtilityList>
           </BlogCard>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -15,8 +15,7 @@ export const projects = [
     image: "/images/madu.png",
     tags: ["React native", "SCSS"],
     source: "https://github.com/joel2035/Application-Madu",
-    visit: "",
-    id: 0,
+    id: 1,
   },
   {
     title: "Moving-media",
@@ -25,7 +24,8 @@ export const projects = [
     image: "/images/moving-media.png",
     tags: ["Javascript", "HTML & SCSS", "PHP", "IONOS"],
     source: "https://github.com/joel2035/Moving-Media",
-    id: 0,
+    visit: "https://movingmedia.fr/"
+    id: 2,
   },
   {
     title: "Xevent Dataviz",
@@ -35,7 +35,7 @@ export const projects = [
     tags: ["Nextjs", "Nestjs", "MySQL"],
     source: "https://github.com/xeventDatavizProject/webapp",
     visit: "https://parkmanagerapp.herokuapp.com/",
-    id: 0,
+    id: 3,
   },
   {
     title: "App location de voiture",
@@ -43,9 +43,7 @@ export const projects = [
     image: "/images/mobile_app.jpeg",
     tags: ["Flutter", "Firebase"],
     source: "#",
-    visit:
-      "https://forum.xda-developers.com/t/app-port-6-0-lenovo-snapit-camera-5-8-53-for-all-devices.3608065/",
-    id: 3,
+    id: 4,
   },
   {
     title: "App de Méditation",
@@ -55,7 +53,7 @@ export const projects = [
     source: "#",
     visit:
       "https://forum.xda-developers.com/t/c-apps-v2-unofficial-6-0-x-cyanogen-os-capps-v2-for-cm13-and-cm13-based-roms.3254865/",
-    id: 3,
+    id: 5,
   },
   {
     title: "My weather",
@@ -63,8 +61,7 @@ export const projects = [
     image: "/images/meteo.jpg",
     tags: ["React", "PWA"],
     source: "https://github.com/joel2035/pwa_react_app",
-    visit: "https://admiring-mestorf-72dc0b.netlify.app/",
-    id: 2,
+    id: 6,
   }
 ];
 
@@ -80,5 +77,5 @@ export const TimeLineData = [
   { year: 2019, text: "Entrepreneur dans le secteur de l'informatique" },
   { year: 2020, text: "Stage Dev Front chez Studely" },
   { year: 2021, text: "Alternant Dev web chez Studely" },
-  { year: 2022, text: "Alternant Dev web chez Studely " },
+  { year: 2023, text: "Alternant Dev web chez Studely " },
 ];
